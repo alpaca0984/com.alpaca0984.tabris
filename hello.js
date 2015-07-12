7f20e8a6d245b5b937725d55c0dc360996af1508
Masataka Hirano <hirano.masataka@a-tm.co.jp>
2015-07-12 02:34:28 +0000

    commit

--- hello.js
+++ hello.js
@@ -1,20 +1,23 @@
 var page = tabris.create("Page", {
-  title: "Hello, World!",
+  title: "Create Memo",
   topLevel: true
 });
 
 var button = tabris.create("Button", {
-  text: "Native Widgets",
-  layoutData: {centerX: 0, top: 100}
+  text: "create TextInput",
+  layoutData: {centerX: 0, bottom: 50}
 }).appendTo(page);
 
-var label = tabris.create("TextView", {
-  font: "24px",
-  layoutData: {centerX: 0, top: [button, 50]}
-}).appendTo(page);
-
-button.on("select", function() {
-  label.set("text", "Totally Rock!");
-});
+var hash = {};
+for (var i = 1; i < 6; i++) {
+  var id = "text_field_input_" + i;
+  var attrTop = (1 < i) ? ["#text_field_input_" + (i - 1), 18] : 18;
+  tabris.create("TextInput", {
+    id: id,
+    message: "text"
+  }).appendTo(page);
+  hash["#" + id] = {layoutData: {left: 15, top: attrTop, width: 240}};
+}
+page.apply(hash);
 
 page.open();
