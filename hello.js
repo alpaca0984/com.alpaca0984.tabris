f3bd2c77d5c246cf5ea9684524593c871bebd659
Masataka Hirano <hirano.masataka@a-tm.co.jp>
2015-07-21 14:23:03 +0000

    modified

--- hello.js
+++ hello.js
@@ -1,24 +1,14 @@
-var page = tabris.create("Page", {
-  title: "Create Memo",
-  topLevel: true
+[
+  // "./modules/ToastPage",
+  // "./modules/BadgePage",
+  // "./modules/MotionPage",
+  // "./modules/DialogPage",
+  // "./modules/NetworkPage",
+  "./cordova/modules/CameraPage",
+  // "./modules/BarcodeScannerPage"
+].forEach(function(page) {
+  require(page).create();
 });
-console.log(window.Connection);
 
-var button = tabris.create("Button", {
-  text: "create TextInput",
-  layoutData: {centerX: 0, bottom: 50}
-}).appendTo(page);
-
-var hash = {};
-for (var i = 1; i < 6; i++) {
-  var id = "text_field_input_" + i;
-  var attrTop = (1 < i) ? ["#text_field_input_" + (i - 1), 18] : 18;
-  tabris.create("TextInput", {
-    id: id,
-    message: "text"
-  }).appendTo(page);
-  hash["#" + id] = {layoutData: {left: 15, top: attrTop, width: 240}};
-}
-page.apply(hash);
-
-page.open();
+tabris.create("Drawer").append(tabris.create("PageSelector"));
+tabris.ui.children("Page")[0].open();
